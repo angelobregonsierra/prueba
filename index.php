e0d88cb9b453a7ea3ebfe905b3f9015dd587eb0e
vanbasten-23 <39516194+vanbasten-23@users.noreply.github.com>
2020-12-16 20:41:31 +0100

    Introduzco conexión a base de datos.

--- index.php
+++ index.php
@@ -6,11 +6,36 @@
     <meta name="title" content="PHP en AWS">
     <meta name="description" content="Página web sobre PHP en AWS">    
   </head>  
-  <body>    
+  <body>
+    <?php
+    $servername = $_SERVER['RDS_HOSTNAME'];
+    $database = $_SERVER['RDS_DB_NAME'];
+    $username = $_SERVER['RDS_USERNAME'];
+    $password = $_SERVER['RDS_PASSWORD'];
+    $conn = mysqli_connect($servername, $username, $password, $database);
+    if (!$conn) {
+        die("Hubo un fallo en la conexión: " . mysqli_connect_error());
+    }
+    echo "Conectados a la base de datos correctamente.";
+    $sql = "CREATE TABLE usuarios (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    apellido VARCHAR(50) NOT NULL,
+    correo VARCHAR(50),
+    fecha TIMESTAMP
+    )";
+
+    if ($conn->query($sql) === TRUE) {
+        echo " La tabla usuarios ha sido creada correctamente.";
+    } else {
+        echo " Hubo un error al crear la tabla usuarios: " . $conn->error;
+    }
+    $conn->close();
+    ?>
     <header>
-      <h1>PHP en AWS</h1>      
-    </header>    
-    <section>      
+      <h1>PHP en AWS</h1>
+    </header>
+    <section>
       <article>
         <p>A continuación se mostrará un número aleatorio (actualice su navegador para ver un nuevo número):
         <strong>
